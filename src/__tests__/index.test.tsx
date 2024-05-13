48bae20cfb97698ac6af2eb7aade5d269338dc92
N Praveen Kumar <praveen@Ns-MacBook-Air.local>
2024-05-13 10:17:35 +0530

    build: jest configured for testing

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,1 +1,12 @@
-it.todo('write a test');
+import React from 'react';
+import { render } from '@testing-library/react-native';
+
+import { Button } from '../../src';
+
+describe('Button', () => {
+  it('renders correctly', () => {
+    const tree = render(<Button>Text Button</Button>).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
