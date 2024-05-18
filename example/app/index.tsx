10147db1cb2e8ab8f9e483308f9c56a84de48f2d
N Praveen Kumar <praveen@Ns-MacBook-Air.local>
2024-05-18 23:06:42 +0530

    fix: eslint configuration

--- example/app/index.tsx
+++ example/app/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Alert, View } from 'react-native';
+import { Alert, View, StyleSheet } from 'react-native';
 import { Button } from 'native-nextui';
 
 const Home = () => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={styles.container}>
       <Button
         onPress={() =>
           Alert.alert('React Native NextUI', 'Hello World!', [
@@ -24,3 +24,11 @@ const Home = () => {
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
